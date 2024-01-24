01fe5577a34a8d237cf2fd163ed1c5b9e666995e
Evann <evann.jacquot@artfx.fr>
2024-01-24 15:12:24 +0100

    [refactor]

--- Assets/BackStab.cs
+++ Assets/BackStab.cs
@@ -1,6 +1,3 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEditor;
 using UnityEngine;
 
 public class BackStab : MonoBehaviour
@@ -24,12 +21,16 @@ public class BackStab : MonoBehaviour
         Vector3 direction = player.transform.position - transform.position;
 
         float dotProduct = Vector3.Dot(transform.forward, direction.normalized);
-        float dotProductDirection = Vector3.Dot(transform.forward.normalized, player.transform.forward.normalized);
+        float dotProductDirection = Vector3.Dot(transform.forward, player.transform.forward);
 
         if (dotProduct < -0.8f && dotProductDirection > angle && direction.magnitude <= maxDistance && characterMovement.instance.GetCanBackStab())
         {
             print("Tah le backstab");
             GetComponent<MeshRenderer>().material = green;
+            if (Input.GetKey(KeyCode.Space))
+            {
+                Destroy(gameObject);
+            }
         }
         else if (dotProduct > seeAngle && direction.magnitude <= seeDistance)
         {
